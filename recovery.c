2430f475db2c41b0ee408899a10de556e5765b85
Bumble-Bee <bigbeeshane@gmail.com>
2012-10-12 02:22:27 +0100

    add : minadbd + sideload

--- recovery.c
+++ recovery.c
@@ -41,6 +41,9 @@
 #include "roots.h"
 #include "recovery_ui.h"
 
+#include "adb_install.h"
+#include "minadbd/adb.h"
+
 #include "extendedcommands.h"
 #include "flashutils/flashutils.h"
 #include "dedupe/dedupe.h"
@@ -732,9 +735,13 @@ prompt_and_wait() {
                 show_advanced_menu();
                 break;
                 
+            case ITEM_SIDELOAD:
+                apply_from_adb();
+                break;
+
             case ITEM_POWEROFF:
                 poweroff = 1;
-                return;
+                return;                
         }
     }
 }
@@ -746,6 +753,12 @@ print_property(const char *key, const char *name, void *cookie) {
 
 int
 main(int argc, char **argv) {
+
+    if (argc == 2 && strcmp(argv[1], "adbd") == 0) {
+        adb_main();
+        return 0;
+    }
+
     // Recovery needs to install world-readable files, so clear umask
     // set by init
     umask(0);
